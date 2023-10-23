d6160ca9f1cd857e716f8df169d59ea215c0a8f8
luyu-wu <luyuwuvk@gmail.com>
2023-10-23 12:58:17 -0400

    Add energy-minimization script

--- calculate_droplet.py
+++ calculate_droplet.py
@@ -2,11 +2,21 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+import pymesh
 
 # Init Code
+
+
+## DROPLET VARIABLES
+
+volume = 1 # mL
+surfaceTension = 72 # mN/m
+
 
 
 pygame.init()
 display = (int(1000),int(1000))
 pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+
 
