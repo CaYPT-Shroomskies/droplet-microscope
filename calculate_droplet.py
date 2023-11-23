ebf42e63b9362c8da3d1d97677a0e32da9a568a6
luyu-wu <luyuwuvk@gmail.com>
2023-11-23 10:52:42 -0500

    minor changes to script

--- calculate_droplet.py
+++ calculate_droplet.py
@@ -14,9 +14,26 @@ surfaceTension = 72 # mN/m
 
 
 
+
+class droplet:
+    def __init__(self,com,sa,energy):
+        droplet.center_of_mass = com;
+        droplet.surface_area = sa;
+        droplet.energy = energy;
+        droplet.vector = [com,energy,sa];
+
+
+def CalculateSurfaceEnergy()
+
+
+def DrawCall(COM,SA_F): # Center_of_mass (cm) (one dimensional due to symetry along other axis), Surface_area_floor (cm^2): amount of droplet touching the floor
+    # Simplfiy Variable
+    energy = ((COM/100)*9.8*(volume/1000))*1000 # energy calculated in mJ
+    COM = center_of_mass
+
+
+for i in range
+
 pygame.init()
 display = (int(1000),int(1000))
 pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-
-
-
