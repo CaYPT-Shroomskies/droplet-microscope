90fcb3b94e1616e167263e942e9635b49ba4c67f
luyu-wu <luyuwuvk@gmail.com>
2023-12-03 19:40:50 -0500

    fixed up gravity and all the errors i think

--- calculate_droplet.py
+++ calculate_droplet.py
@@ -15,36 +15,45 @@ Parabaloid visualization
 
 '''
 
-## DROPLET VARIABLES
+## CONSTANTS ##
 
-volume = 1 # mL
-sl_se_constant = 100 # Surface energy between solid phase and liquid, measured in mJ/cm^2
-gl_se_constant = 72 # Surface energy between liquid and gas phase, measured in mJ/cm^2
+volume = 10 # mL
+
+# [[MEASURED IN J/m^2 BC ITS STANDARD]]
+sl_se_constant = -0.07 # Surface energy between solid phase and liquid, measured in J/m^2
+gl_se_constant = 0.072 # Surface energy between liquid and gas phase, measured in J/m^2
+
+gravity = 9.81 # cm/s^s
 
 
-## PRECISION VARIABLES
-initialShape = 0.25/volume # initial [a] value for the paraboloid
-initialStepSize = 0.05
+## PRECISION VARIABLES ##
+initialShape = - 0.5/volume # initial [a] value for the paraboloid
+initialStepSize = 0.05/volume
 stepSizeGeometry = 0.5 # amount to multiply step value by at each split.
-watchdogTimeoutThreshold = 10000 # Timeout counter
-precision = 3 # Amount of geometric decreases in step size
+watchdogTimeoutThreshold = 100000 # Timeout counter
+precision = 4 # Amount of geometric decreases in step size [Dont go over 50, FP limits are hit far below that]
 
 
-gravitationalPrecision = 100
-arcPrecision = 200 # amount of arcs represented in quarter parabola multiplied into area.
+gravitationalPrecision = 50
+arcPrecision = 100 # amount of arcs represented in quarter parabola multiplied into area.
+
+# Unit Conversions
+gl_se_constant *= (1000 / 10000) # mJ/J and cm^2/m^2
+sl_se_constant *= (1000 / 10000) # mJ/J and cm^2/m^2
 
 
-## FUNCTIONS
+## FUNCTIONS ##
 
 def floorEnergy(a,c):
-    return - sl_se_constant * (c/a) * math.pi; # insert an actual explanation here cause yes :sob:
+    #print((sl_se_constant * (-c/a) * math.pi))
+    return (sl_se_constant * (-c/a) * math.pi)
 
 def gasPhaseEnergy(a,c):
     # get the surface area through a line integral of the length of the parabolic curve
 
-    radius = math.sqrt(c/a) # Radius of base of paraboloid
+    radius = math.sqrt(-c/a) # Radius of base of paraboloid
     segment_length = radius/arcPrecision
-    
+
     area = 0
     for n in range(1,arcPrecision):
         # just the pythagagorean of segment length and delta y, multiplied by circumference to get the surface area
@@ -52,12 +61,13 @@ def gasPhaseEnergy(a,c):
 
     return area * gl_se_constant
 
-def gravitationalEnergyIntegral(a,c): # a and c belong to the standard equation form ax^2 + c. k is the precision value.
+
+def gravitationalEnergy(a,c): # a and c belong to the standard equation form ax^2 + c. k is the precision value.
     sum = 0
     k = gravitationalPrecision
     for n in range(k):
-        sum -= ( (math.pi)*(c**3)*(n**2)*(0.098) ) / ( (k**3)*a )  # volume multiplied by height and gravitational constant
-    print("Eg = "+str(sum))
+        sum -= ( (math.pi)*(c**3)*(n**2)*(gravity/100) ) / ( (k**3)*a )  # volsume multiplied by height and gravitational constant
+    print("Eg "+str(sum))
     return sum
 
 def drawParaboloid(a):
@@ -65,18 +75,18 @@ def drawParaboloid(a):
     # just set the integral to be equal to the volume and solve for it :pleading_face: bro the 3d integral tho
 
     # nvm we just do the funny thing with the paraboloid equation
-    return math.sqrt(volume*2*a/math.pi)
+    return math.sqrt( - volume*2*a/math.pi )
 
 
 def getEnergy(a):
     c = drawParaboloid(a)
-    e = gravitationalEnergyIntegral(a,c) + floorEnergy(a,c) + gasPhaseEnergy(a,c)
+    e = gravitationalEnergy(a,c) + floorEnergy(a,c) + gasPhaseEnergy(a,c)
     return e,c
 
 
-## RUNTIME
+## RUNTIME ##
 
-# Runtime Variables
+# Runtime Variables #
 watchdog = 0
 geometricRepeats = 0
 lastShape = initialShape
@@ -88,6 +98,7 @@ stepSize = initialStepSize
 # Determine Initial direction
 lastEnergy,_ = getEnergy(lastShape)
 upEnergy,_ = getEnergy(lastShape+stepSize)
+
 if upEnergy > lastEnergy:
     stepSize *= -1 # If a positive step increases energy, reflect step.
 
@@ -96,11 +107,12 @@ while watchdog < watchdogTimeoutThreshold and geometricRepeats < precision:
     a = lastShape+stepSize
     stepEnergy,c = getEnergy(a)
 
-    graph.append([stepEnergy,a,c]) # Energy, a, and c
+    graph.append([stepEnergy,a,c]) # record e, a, and c
 
     if stepEnergy > lastEnergy: # If energy is greater than the last step, reverse direction and make step smaller
         stepSize *= -(stepSizeGeometry)
         geometricRepeats += 1
+
     #print(str(stepEnergy)+" mJ" )
     lastEnergy,lastShape = stepEnergy,a # Update runtime values for next iteration
 
@@ -114,7 +126,15 @@ if geometricRepeats >= precision:
 else:
     print("Watchdog timed out.")
 
-exit()
 
-#plt.plot(np.array(graph))
+a_list = []
+e_list = []
+for i in graph:
+    e_list.append(graph[0])
+
+    a_list.append(graph[1])
+
+#plt.plot(np.array(a_list),np.array(e_list))
 #plt.show()
+
+#exit()
